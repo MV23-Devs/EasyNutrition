224ed5c9525b0328c031e304b696549b676b0674
saarangbond <48871052+saarangbond@users.noreply.github.com>
2020-12-25 17:38:41 -0800

    meal selection and food groups changes

--- res/js/foodgroups.js
+++ res/js/foodgroups.js
@@ -1,9 +1,7 @@
-const { Console } = require("console");
-
-let proteinItems = ["Meat", "Plant-Based"];
+let proteinItems = ["redMeats", "whiteMeats", "fish", "nuts"];
 let fruitItems = ["Berry"];
 let vegetableItems = ["Pepper"];
-let grainItems = ["Bread"];
+let grainItems = ["bread", "pasta", "rice"];
 let dairyItems = ["Milk", "Cheese"];
 
 let foodGroups = {
@@ -14,7 +12,26 @@ let foodGroups = {
     "Dairy": dairyItems
 };
 
-//let meal = [];
+let myLink = document.getElementById('submitSelection');
+myLink.onclick = function(){
+    let mealInp = []
+    let groups = ["protein", "grain"]
+    for(let i=0; i < groups.length; i++){
+        for(let j=1; j < 4; j++){
+            if(document.getElementById(String(groups[i]) + String(j)).checked){
+                mealInp.push(document.getElementById(String(groups[i]) + String(j)).value);
+            }
+        }
+    }
+    let displayMessage = checkMissing(mealInp);
+    console.log(displayMessage);
+    document.getElementById("displayText").innerHTML = displayMessage;
+    document.getElementById("continueButtonDiv").innerHTML = "<button id='continueButton'><a href='recipes.html'>Continue to Recipes</a></button>";
+    return false;
+}
+
+//function takes in an array of items in meal and returns a string message with all the missing groups
+//need to fetch data from form and then take string output and display it
 function checkMissing(meal){
     //parameter meal is an array of all items in the meal
     let groupCheck = {
@@ -32,10 +49,16 @@ function checkMissing(meal){
             }
         }
     }
-    return groupCheck;
+    let message = "You need to include some of the following food groups in your next meal: "
+    for(let i = 0; i < keys.length; i++){
+        if(groupCheck[keys[i]] == false){
+            message += keys[i] + " ";
+        }
+    }
+    return message;
 }
 
-//let mealEx = ["Meat", "Berry", "Pepper", "Bread", "Milk", "Cheese"];
+//let mealEx = ["Meat", "Pepper", "Bread", "Milk", "Cheese"];
 //console.log(checkMissing(mealEx));
 
-console.log(document.getElementById("mealSelector"));
+//console.log(document.getElementById("mealSelector"));
