35b8349eb88b6dbefe3aa11c5f9247325f6950e0
Jacob Ismael <jacob.ismael@gmail.com>
2020-12-25 19:05:01 -0800

    started doing shit

--- res/js/foodgroups.js
+++ res/js/foodgroups.js
@@ -5,34 +5,46 @@ let grainItems = ["bread", "pasta", "rice"];
 let dairyItems = ["milk", "cheese", "ogurt"];
 
 let foodGroups = {
-    "Protein": proteinItems, 
-    "Fruit": fruitItems, 
-    "Vegetable": vegetableItems, 
-    "Grain": grainItems, 
+    "Protein": proteinItems,
+    "Fruit": fruitItems,
+    "Vegetable": vegetableItems,
+    "Grain": grainItems,
     "Dairy": dairyItems
 };
 
 let myLink = document.getElementById('submitSelection');
-myLink.onclick = function(){
+myLink.onclick = function () {
     let mealInp = []
     let groups = ["protein", "grain", "dairy", "vegetables", "fruits"]
-    for(let i=0; i < groups.length; i++){
-        for(let j=1; j < 4; j++){
-            if(document.getElementById(String(groups[i]) + String(j)).checked){
+    for (let i = 0; i < groups.length; i++) {
+        for (let j = 1; j < 4; j++) {
+            if (document.getElementById(String(groups[i]) + String(j)).checked) {
                 mealInp.push(document.getElementById(String(groups[i]) + String(j)).value);
             }
         }
     }
     console.log(mealInp)
-    let displayMessage = checkMissing(mealInp);
-    document.getElementById("displayText").innerHTML = displayMessage;
+    let result = checkMissing(mealInp);
+    $("#needs").empty();
+    for (let i = 0; i < result.length; i++) {
+        let ul = document.getElementById("needs");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(result[i]));
+        ul.appendChild(li);
+    }
+    if(result.length > 0) {
+        document.getElementById("displayText").innerHTML = "You need to include some of the following food groups in your next meal: ";
+    } else {
+        document.getElementById("displayText").innerHTML = "Well Done, all the food groups are present in your meal!";
+    }
     document.getElementById("continueButtonDiv").innerHTML = "<p>Checkout more recipes here:</p><a href='recipes.html'>Continue to Recipes</a>";
+    document.getElementById("results").style["display"] = "block";
     return false;
 }
 
 //function takes in an array of items in meal and returns a string message with all the missing groups
 //need to fetch data from form and then take string output and display it
-function checkMissing(meal){
+function checkMissing(meal) {
     //parameter meal is an array of all items in the meal
     let groupCheck = {
         "Protein": false,
@@ -42,25 +54,21 @@ function checkMissing(meal){
         "Dairy": false
     }
     keys = Object.keys(foodGroups)
-    for(let i=0; i < keys.length; i++){
-        for(let j=0; j < meal.length; j++){
-            if(foodGroups[keys[i]].includes(meal[j])){
+    for (let i = 0; i < keys.length; i++) {
+        for (let j = 0; j < meal.length; j++) {
+            if (foodGroups[keys[i]].includes(meal[j])) {
                 groupCheck[keys[i]] = true;
             }
         }
     }
-    let counter = 0;
-    let message = "You need to include some of the following food groups in your next meal: "
-    for(let i = 0; i < keys.length; i++){
-        if(groupCheck[keys[i]] == false){
-            message += keys[i] + " ";
-            counter++;
+    let result = []
+    for (let i = 0; i < keys.length; i++) {
+        if (groupCheck[keys[i]] == false) {
+            result.push(keys[i]);
         }
     }
-    if(counter == 0) {
-        message = "Well Done, all the food groups are present in your meal!"
-    }
-    return message;
+
+    return result;
 }
 
 //let mealEx = ["Meat", "Pepper", "Bread", "Milk", "Cheese"];
